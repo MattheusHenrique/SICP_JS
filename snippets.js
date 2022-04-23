6d86497dcfc26fe60b9081904b85721e44d1f32a
Ma77heus <58952630+MattheusHenrique@users.noreply.github.com>
2022-04-23 11:39:47 -0300

    Create snippets.js

---
+++ snippets.js
@@ -0,0 +1,7 @@
+//Função com uso legal
+//Retorna um funçao de acordo com o valor de b
+function plus(a, b) { return a + b; }
+function minus(a, b) { return a - b; }
+function a_plus_abs_b(a, b) {
+    return (b >= 0 ? plus : minus)(a, b)
+}
